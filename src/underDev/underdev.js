58c94d8aa048fbcdf139b625eb5f1e445d9eb891
ShashankKawle <shashankkawle67@gmail.com>
2020-10-17 16:08:51 +0530

    update

--- src/underDev/underdev.js
+++ src/underDev/underdev.js
@@ -11,7 +11,7 @@ class UnderDev extends Component
         return(
             <div className="underdev-box">
                 <h5><strong>{messageStr}</strong></h5>
-                <img src={underDevelopImage} alt="Under Development Image" width="20%" height="20%"/>
+                <img src={underDevelopImage}  alt="bot" width="20%" height="20%"/>
             </div>
         )
     }
